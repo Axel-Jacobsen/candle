6a1d491fddb5db97a34397845807f74ecd0d0c62
Axel Jacobsen <axelnj44@gmail.com>
2024-07-20 15:59:25 -0700

    Flip flop impl

---
+++ README.md
@@ -0,0 +1,21 @@
+# Lamp - it's happening this time
+
+Goal: Blow on windmill-like lamp, moving it's blades. The rotation of the blades turns on or turns off the lamp.
+
+Requirements:
+  1 cute
+  2 should be able to rotate the blades with a "reasonable blow"
+    - shouldn't make you pass out
+    - shouldn't take more than one try
+    - shouldn't be easy enough to accidentally happen
+  3 all circuitry, no computer
+  4 nice case, cool looking design, see requirement 1
+
+## EE
+
+### Detecting Rotations
+
+How do we actually convert rotation
+
+- Hall Effect sensors
+- Magnet and coil of wire
--- spice/waveforms/waveform.py
+++ spice/waveforms/waveform.py
@@ -2,6 +2,7 @@
 
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 
 from pathlib import Path
 from typing import Optional
@@ -14,12 +15,23 @@ def save_as_waveform_file(t: np.array, v: np.array, output_path: Optional[Path] 
             f.write(f"{tt} {vv}\n")
 
 
-
 if __name__ == "__main__":
-    parser = argarse.ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument("--output-path", type=Path)
+    parser.add_argument("--plot", action="store_true", default=False)
     args = parser.parse_args()
 
-    t = np.linspace(0, 3, 10000)
-    v = 2 * np.exp(-2 * t) * np.sin(10 * np.pi * t)
+    # https://www.desmos.com/calculator/lvxrwjouzm
+    A = 1.6
+    f = 100
+    s = 1
+    m = -0.3
+
+    t = np.linspace(0.0001, 5, 10000)
+    v = A * np.power(np.sin(f * t), 2) * np.exp(-np.power(np.log(t) - m, 2) / (2 * s ** 2)) / (t * s * np.sqrt(2 * np.pi))
+
+    if args.plot:
+        plt.plot(t,v)
+        plt.show()
+
     save_as_waveform_file(t, v, output_path=args.output_path)
